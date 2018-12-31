338a95095197169694eafa7618674731cae28e75
HyungMin Kim <pilnine@gmail.com>
2018-12-31 13:31:24 -0800

    Add Script

---
+++ 301_MR_Translation/Assets/Scripts/MicrophoneManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class MicrophoneManager : MonoBehaviour {
+
+    // Help to access instance of this object 
+    public static MicrophoneManager instance;
+
+    // AudioSource component, provides access to mic 
+    private AudioSource audioSource;
+
+    // Flag indicating mic detection 
+    private bool microphoneDetected;
+
+    // Component converting speech to text 
+    private DictationRecognizer dictationRecognizer;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Use Unity Microphone class to detect devices and setup AudioSource 
+        if (Microphone.devices.Length > 0)
+        {
+            Results.instance.SetMicrophoneStatus("Initialising...");
+            audioSource = GetComponent<AudioSource>();
+            microphoneDetected = true;
+        }
+        else
+        {
+            Results.instance.SetMicrophoneStatus("No Microphone detected");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    private void Awake()
+    {
+        // Set this class to behave similar to singleton 
+        instance = this;
+    }
+
+    public void StartCapturingAudio()
+    {
+        if (microphoneDetected)
+        {
+            // Start dictation 
+            dictationRecognizer = new DictationRecognizer();
+            dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+            dictationRecognizer.Start();
+
+            // Update UI with mic status 
+            Results.instance.SetMicrophoneStatus("Capturing...");
+        }
+    }
+
+    /// <summary> 
+    /// Stop microphone capture. Debugging message is delivered to the Results class. 
+    /// </summary> 
+    public void StopCapturingAudio()
+    {
+        Results.instance.SetMicrophoneStatus("Mic sleeping");
+        Microphone.End(null);
+        dictationRecognizer.DictationResult -= DictationRecognizer_DictationResult;
+        dictationRecognizer.Dispose();
+    }
+
+}

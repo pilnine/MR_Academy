5608070b760f8d7989f4e7d29a501aaa604dda95
HyungMin Kim <pilnine@gmail.com>
2019-01-14 15:49:09 -0800

    304_FaceCognition

---
+++ 304_FaceRecognition/Assets/Scripts/FaceAnalysis.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FaceAnalysis : MonoBehaviour {
+    /// <summary>
+    /// Allows this class to behave like a singleton
+    /// </summary>
+    public static FaceAnalysis Instance;
+
+    /// <summary>
+    /// The analysis result text
+    /// </summary>
+    private TextMesh labelText;
+
+    /// <summary>
+    /// Bytes of the image captured with camera
+    /// </summary>
+    internal byte[] imageBytes;
+
+    /// <summary>
+    /// Path of the image captured with camera
+    /// </summary>
+    internal string imagePath;
+
+    /// <summary>
+    /// Base endpoint of Face Recognition Service
+    /// </summary>
+    const string baseEndpoint = "https://westus.api.cognitive.microsoft.com/face/v1.0/";
+
+    /// <summary>
+    /// Auth key of Face Recognition Service
+    /// </summary>
+    private const string key = "2801f6443faf4c1b8adcaa3eccaae562";
+
+    /// <summary>
+    /// Id (name) of the created person group 
+    /// </summary>
+    private const string personGroupId = "1";
+
+}
+
+/// <summary>
+/// The Person Group object
+/// </summary>
+public class Group_RootObject
+{
+    public string personGroupId { get; set; }
+    public string name { get; set; }
+    public object userData { get; set; }
+}
+
+/// <summary>
+/// The Person Face object
+/// </summary>
+public class Face_RootObject
+{
+    public string faceId { get; set; }
+}
+
+/// <summary>
+/// Collection of faces that needs to be identified
+/// </summary>
+public class FacesToIdentify_RootObject
+{
+    public string personGroupId { get; set; }
+    public List<string> faceIds { get; set; }
+    public int maxNumOfCandidatesReturned { get; set; }
+    public double confidenceThreshold { get; set; }
+}
+
+/// <summary>
+/// Collection of Candidates for the face
+/// </summary>
+public class Candidate_RootObject
+{
+    public string faceId { get; set; }
+    public List<Candidate> candidates { get; set; }
+}
+
+public class Candidate
+{
+    public string personId { get; set; }
+    public double confidence { get; set; }
+}
+
+/// <summary>
+/// Name and Id of the identified Person
+/// </summary>
+public class IdentifiedPerson_RootObject
+{
+    public string personId { get; set; }
+    public string name { get; set; }
+}

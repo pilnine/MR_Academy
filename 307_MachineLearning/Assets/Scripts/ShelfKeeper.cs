959d7b880336f005c416db3eb1357855f9092feb
HyungMin Kim <pilnine@gmail.com>
2019-01-18 17:42:25 -0800

    307_ML

---
+++ 307_MachineLearning/Assets/Scripts/ShelfKeeper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ShelfKeeper : MonoBehaviour
+{
+    /// <summary>
+    /// Provides this class Singleton-like behaviour
+    /// </summary>
+    public static ShelfKeeper instance;
+
+    /// <summary>
+    /// Unity Inspector accessible Reference to the Text Mesh object needed for data
+    /// </summary>
+    public TextMesh dateText;
+
+    /// <summary>
+    /// Unity Inspector accessible Reference to the Text Mesh object needed for time
+    /// </summary>
+    public TextMesh timeText;
+
+    /// <summary>
+    /// Provides references to the spawn locations for the products prefabs
+    /// </summary>
+    public Transform[] spawnPoint;
+}
+
